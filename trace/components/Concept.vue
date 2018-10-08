0d0cd02a9f1d6b3a3ad0e1d2b28026d5807e4691
yinm <yinm.cminor9@gmail.com>
2018-10-08 11:16:27 +0900

    Try 2-2-2

--- trace/components/Concept.vue
+++ trace/components/Concept.vue
@@ -1,17 +1,36 @@
 <template>
   <section class="conB">
     <div class="container">
-      <i class="far fa-chart-bar"/>
+      <i class="far fa-chart-bar chart"/>
       <div class="text">
         <h2>ライフログって何？</h2>
         <p>日常のあれこれを記録していくライフログ。いろいろなものが見えてきます。</p>
-        <a href="#">MORE...</a>
+        <a href="#">
+          MORE...<i class="fas fa-chevron-right chevron" />
+        </a>
       </div>
     </div>
   </section>
 </template>
 
 <style scoped>
+  .conB {
+    text-align: center;
+  }
+
+  .conB .chart {
+    margin-bottom: 20px;
+    padding: 15px;
+    border-radius: 50%;
+    background-color: #ddd;
+    font-size: 3rem;
+    color: #fff;
+  }
+
+  .conB .chevron {
+    margin-left: 5px;
+  }
+
   .conB h2 {
     margin-top: 0;
     margin-bottom: 10px;
