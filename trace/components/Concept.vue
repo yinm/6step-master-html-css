ea4469e6d708ef5c937cbdb341f5702e07b9f2e5
yinm <yinm.cminor9@gmail.com>
2018-10-08 09:21:06 +0900

    Check sample

--- trace/components/Concept.vue
+++ trace/components/Concept.vue
@@ -1,26 +1,36 @@
 <template>
   <section class="conB">
     <div class="container">
-      <h2>ライフログって何？</h2>
-      <p>日常のあれこれを記録していくライフログ。いろいろなものが見えてきます。</p>
-      <a href="#">MORE...</a>
+      <div class="text">
+        <h2>ライフログって何？</h2>
+        <p>日常のあれこれを記録していくライフログ。いろいろなものが見えてきます。</p>
+        <a href="#">MORE...</a>
+      </div>
     </div>
   </section>
 </template>
 
 <style scoped>
   .conB h2 {
-    margin: 0;
+    margin-top: 0;
+    margin-bottom: 10px;
     font-size: 20px;
   }
 
   .conB p {
-    margin: 13px 0 22px;
+    margin-top: 0;
+    margin-bottom: 20px;
     font-size: 14px;
-    color: #555;
+    line-height: 1.8;
+    opacity: 0.8;
   }
 
   .conB a {
     color: var(--main-color);
+    text-decoration: none;
+  }
+
+  .conB a:hover {
+    text-decoration: underline;
   }
 </style>
