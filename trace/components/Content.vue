2a54252b973ee94c7b10ff3ae1058f2c2a2ac08c
yinm <yinm.cminor9@gmail.com>
2018-10-09 22:17:51 +0900

    Check sample

--- trace/components/Content.vue
+++ trace/components/Content.vue
@@ -1,11 +1,9 @@
 <template>
   <section class="conC">
     <div class="container">
-      <div class="img" />
-      <div class="text">
-        <h2>面倒な設定やデータの管理は全部おまかせ</h2>
-        <p>ライフログでつまずきがちな面倒な設定やデータ管理の煩わしさはありません。他のログアプリとの連携もワンクリックで完了です。</p>
-      </div>
+      <div class="photo" />
+      <h2>面倒な設定やデータの管理は全部おまかせ</h2>
+      <p>ライフログでつまずきがちな面倒な設定やデータ管理の煩わしさはありません。他のログアプリとの連携もワンクリックで完了です。</p>
       <a href="#">
         詳しい設定方法を確認する
         <i class="fas fa-chevron-right" />
@@ -20,35 +18,26 @@
     color: var(--text-bright-color);
   }
 
-  .conC .img {
-    height: 200px;
-    width: 100%;
-    background-image: url("/note.jpg");
-    background-size: cover;
-    background-position: center;
-  }
-
-  .conC .text {
-    padding-bottom: 42px;
-  }
-
-  .conC .text h2 {
+  .conC h2 {
     margin-top: 0;
-    margin-bottom: 12px;
+    margin-bottom: 10px;
     font-size: 20px;
   }
 
-  .conC .text p {
-    margin: 0;
+  .conC p {
+    margin-top: 0;
+    margin-bottom: 20px;
     font-size: 14px;
+    line-height: 1.8;
     opacity: 0.8;
   }
 
   .conC a {
     display: inline-block;
-    border: 3px solid var(--text-bright-color);
-    border-radius: 6px;
+    margin-top: 20px;
     padding: 10px 30px;
+    border: solid 3px currentColor;
+    border-radius: 6px;
     background-color: var(--accent-color);
     color: var(--text-bright-color);
     font-size: 14px;
@@ -57,8 +46,15 @@
 
   .conC a:hover {
     background-image: linear-gradient(
-      rgba(255, 255, 255, 0.3),
-      rgba(255, 255, 255, 0.3)
+      rgba(255, 255, 255, 0.2),
+      rgba(255, 255, 255, 0.2)
     );
+  }
+
+  .conC .photo {
+    min-height: 200px;
+    background-image: url("/note.jpg");
+    background-position: center;
+    background-size: cover;
   }
 </style>
