dc60d38eece80e99478bf9bf62b4b14bb0dfda81
yinm <yinm.cminor9@gmail.com>
2018-10-16 21:45:28 +0900

    Try 3-1-1

---
+++ trace/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <h2>LOGGER</h2>
+      <p>〒000-0000 東京都中央区杉並3-3-3-403</p>
+      <a href="#">http://logger.nett/</a>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+  @import url('https://fonts.googleapis.com/css?family=Montserrat');
+
+  .footer h2 {
+    margin-top: 0;
+    margin-bottom: 10px;
+    font-family: 'Montserrat', sans-serif;
+    font-size: 30px;
+    letter-spacing: 0.2em;
+  }
+
+  .footer p {
+    margin: 0;
+    font-size: 14px;
+  }
+
+  .footer a {
+    text-decoration: none;
+    font-size: 14px;
+    color: #000;
+  }
+</style>
--- trace/pages/index.vue
+++ trace/pages/index.vue
@@ -4,6 +4,7 @@
     <Concept />
     <Content />
     <ReversedContent />
+    <Footer />
   </div>
 </template>
 
@@ -12,13 +13,15 @@ import Hero from '~/components/Hero.vue'
 import Concept from '~/components/Concept.vue'
 import Content from '~/components/Content.vue'
 import ReversedContent from '~/components/ReversedContent.vue'
+import Footer from '~/components/Footer.vue'
 
 export default {
   components: {
     Hero,
     Concept,
     Content,
-    ReversedContent
+    ReversedContent,
+    Footer
   }
 }
 </script>
