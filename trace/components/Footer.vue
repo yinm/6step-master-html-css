7382c4474256ecb0ecf517e4264235092aa795f6
yinm <yinm.cminor9@gmail.com>
2018-10-21 08:46:19 +0900

    Try 3-2-1

--- trace/components/Footer.vue
+++ trace/components/Footer.vue
@@ -8,6 +8,24 @@
           <a href="#">http://logger.nett/</a>
         </p>
       </div>
+
+      <div class="footB">
+        <h3 class="list-title">ABOUT</h3>
+        <ul>
+          <li>
+            <a href="#">設立</a>
+          </li>
+          <li>
+            <a href="#">所在地</a>
+          </li>
+          <li>
+            <a href="#">地図</a>
+          </li>
+          <li>
+            <a href="#">スタッフ</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </footer>
 </template>
@@ -41,5 +59,34 @@
   .footA a {
     color: inherit;
     text-decoration: none;
+  }
+
+  .footB .list-title {
+    margin-top: 30px;
+    margin-bottom: 0;
+    border-bottom: 1px solid currentColor;
+    font-size: 14px;
+  }
+
+  .footB ul {
+    margin-top: 10px;
+    margin-bottom: 20px;
+    padding: 0;
+    list-style-type: none;
+  }
+
+  .footB li a {
+    display: block;
+    padding: 5px;
+    font-size: 12px;
+    color: var(--text-bright-color);
+    text-decoration: none;
+  }
+
+  .footB li a:hover {
+    background-image: linear-gradient(
+      rgba(0, 0, 0, 0.3),
+      rgba(0, 0, 0, 0.3)
+    );
   }
 </style>
