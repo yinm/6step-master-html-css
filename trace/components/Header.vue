dad3cc372c341b232113150161f2098c892be62a
yinm <yinm.cminor9@gmail.com>
2018-10-22 07:56:53 +0900

    Check sample

--- trace/components/Header.vue
+++ trace/components/Header.vue
@@ -1,29 +1,35 @@
 <template>
-  <header class="header">
+  <header>
     <div class="container">
-      <h2>LOGGER</h2>
+      <a
+        href="index.html"
+        class="headA"
+      >
+        LOGGER
+      </a>
     </div>
   </header>
 </template>
 
 <style scoped>
-  .container {
-    background-image: linear-gradient(
-      rgba(255, 255, 255, 0.3),
-      rgba(255, 255, 255, 0.3)
-    );
+  header {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 100;
+    width: 100%;
+    background-color: rgba(255, 255, 255, 0.9);
   }
 
-  .header h2 {
+  .headA {
     display: inline-block;
-    margin: 0;
-    height: 70px;
-    width: 144px;
     line-height: 70px;
+    padding-left: 20px;
+    padding-right: 20px;
     background-color: var(--main-color);
     color: var(--text-bright-color);
-    text-align: center;
     font-family: 'Montserrat', sans-serif;
-    font-weight: normal;
+    font-size: 24px;
+    text-decoration: none;
   }
 </style>
