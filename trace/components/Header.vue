87dfa0e068fa85443c7f4b69582bb23bcaf62e84
yinm <yinm.cminor9@gmail.com>
2018-10-22 18:43:30 +0900

    Try 3-6-1

--- trace/components/Header.vue
+++ trace/components/Header.vue
@@ -7,6 +7,21 @@
       >
         LOGGER
       </a>
+
+      <ul class="header-list">
+        <li>
+          <a href="#">トップ</a>
+        </li>
+        <li>
+          <a href="#">コンテンツ</a>
+        </li>
+        <li>
+          <a href="#">ABOUT</a>
+        </li>
+        <li>
+          <a href="#">お問い合わせ</a>
+        </li>
+      </ul>
     </div>
   </header>
 </template>
@@ -31,5 +46,23 @@
     font-family: 'Montserrat', sans-serif;
     font-size: 24px;
     text-decoration: none;
+  }
+
+  .header-list {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+
+  .header-list a {
+    display: block;
+    padding: 15px;
+    text-decoration: none;
+    color: inherit;
+    font-size: 12px;
+  }
+
+  .header-list a:hover {
+    background-color: rgba(0, 0, 0, 0.3);
   }
 </style>
