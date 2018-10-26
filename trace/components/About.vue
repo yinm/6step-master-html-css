51e5c902c1ab500f6e2512890b7fb2f79d52bfa2
yinm <yinm.cminor9@gmail.com>
2018-10-26 20:41:12 +0900

    Try 4-2-1

---
+++ trace/components/About.vue
@@ -0,0 +1,51 @@
+<template>
+  <main>
+    <div class="container">
+      <h2 class="about-title">LOGGERについて</h2>
+      <p class="about-description">LOGGERはライフログに関する情報の発信、アプリ開発、データの分析、ユーザーサポートを行うために設立された会社です。ライフログのサービスやデータを活用することで、日々の生活を楽しみ、よりよくしていくことを目指しています。</p>
+      <p class="about-description">近年はグローバルワールドネットワークとも提携し、国内のみならず、海外展開も含めて活動の場をひろげています。</p>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  .container {
+    max-width: var(--middle-width);
+    margin-left: auto;
+    margin-right: auto;
+    padding: 30px 10px;
+  }
+
+  body {
+    font-size: 16px;
+  }
+
+  @media (max-width: 599px) {
+    :root {
+      font-size: 14px;
+    }
+  }
+
+  @media (min-width: 600px) and (max-width: 799px) {
+    :root {
+      font-size: 16px;
+    }
+  }
+
+  @media (min-width: 800px) {
+    :root {
+      font-size: 18px;
+    }
+  }
+
+  .about-title {
+    padding-left: 0.5rem;
+    border-left: solid 0.75rem var(--accent-color);
+    font-size: 2rem;
+  }
+
+  .about-description {
+    font-size: 1rem;
+    line-height: 2;
+  }
+</style>
---
+++ trace/pages/about.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Header :is-hero-image="false" />
+    <About />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import About from '~/components/About'
+import Header from '~/components/Header'
+import Footer from '~/components/Footer'
+
+export default {
+  components: {
+    About,
+    Footer,
+    Header,
+  }
+}
+</script>
+
+<style>
+  body {
+    margin: 0;
+    font-family: 'メイリオ', 'Hiragino Kaku Gothic Pro', sans-serif;
+  }
+</style>
