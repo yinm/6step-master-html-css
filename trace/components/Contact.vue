e56b394d6cdcb244b40fea3c40e2b2789c54b767
yinm <yinm.cminor9@gmail.com>
2018-10-28 15:07:34 +0900

    Check sample

--- trace/components/Contact.vue
+++ trace/components/Contact.vue
@@ -1,22 +1,24 @@
 <template>
-  <section>
+  <article class="post">
     <div class="container">
-      <h2>お問い合わせ</h2>
+      <h1>お問い合わせ</h1>
       <p>ROGGERについてのご質問などがございましたら、下記の窓口よりお問い合わせください。</p>
-      <div class="cards">
-        <div class="card">
+
+      <div class="contact-wrap">
+        <div class="contact">
           <i class="fas fa-phone" />
-          <h3>電話</h3>
+          <h2>電話</h2>
           <a href="tel:000-1234-5678">000-1234-5678</a>
         </div>
-        <div class="card">
+
+        <div class="contact">
           <i class="fas fa-envelope" />
-          <h3>Eメール</h3>
+          <h2>Eメール</h2>
           <a href="mailto:contact@logger.nett">contact@logger.nett</a>
         </div>
       </div>
     </div>
-  </section>
+  </article>
 </template>
 
 <style scoped>
@@ -47,21 +49,9 @@
     :root {
       font-size: 18px;
     }
-
-    .cards {
-      display: flex;
-    }
-
-    .card {
-      flex: 1;
-    }
-
-    .card:not(:last-child) {
-      margin-right: 20px;
-    }
   }
 
-  h2 {
+  h1 {
     padding-left: 0.5rem;
     border-left: solid 0.75rem var(--accent-color);
     font-size: 2rem;
@@ -72,30 +62,48 @@
     line-height: 2;
   }
 
-  .card {
+  .contact {
     margin-top: 20px;
-    padding: 1.3rem;
+    margin-bottom: 20px;
+    padding: 20px;
     border: solid 1px var(--gray-color);
     text-align: center;
   }
 
-  .card i {
-    padding: 1.1rem;
-    border-radius: 50%;
-    background-color: var(--main-color);
-    color: var(--text-bright-color);
+  .contact i {
+    display: inline-block;
+    margin-bottom: 20px;
     font-size: 2.2rem;
+    width: 2em;
+    line-height: 2em;
+    border-radius: 50%;
+    text-align: center;
+    background-color: var(--main-color);
+    color: var(--icon-color);
   }
 
-  .card h3 {
-    margin-top: 17px;
-    margin-bottom: 17px;
+  .contact h2 {
+    margin-top: 0;
     font-size: 1.2rem;
   }
 
-  .card a {
+  .contact a {
     font-size: 1.4rem;
     color: inherit;
     text-decoration: none;
+  }
+
+  @media (min-width: 768px) {
+    .contact-wrap {
+      display: flex;
+    }
+
+    .contact {
+      flex: 1;
+    }
+
+    .contact:first-child {
+      margin-right: 20px;
+    }
   }
 </style>
