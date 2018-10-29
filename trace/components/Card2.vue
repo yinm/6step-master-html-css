4f06c32d76c38717d56f756a618bcf5b01c41745
yinm <yinm.cminor9@gmail.com>
2018-10-30 08:13:18 +0900

    Try 5-2

---
+++ trace/components/Card2.vue
@@ -0,0 +1,64 @@
+<template>
+  <article>
+    <a href="#">
+      <div
+        :style="{ backgroundImage: 'url(' + thumbSrc + ')' }"
+        class="photo"
+      />
+      <div class="text">
+        <slot name="title" />
+        <slot name="description" />
+      </div>
+    </a>
+  </article>
+</template>
+
+<script>
+  export default {
+    props: {
+      thumbSrc: {
+        type: String,
+        default: ''
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  article {
+    flex: 1 1 500px;
+    display: flex;
+  }
+
+  a {
+    display: flex;
+    margin: 10px;
+    border: solid 1px var(--gray-color);
+    color: inherit;
+    text-decoration: none;
+  }
+
+  a:hover {
+    opacity: 0.8;
+  }
+
+  .photo {
+    min-height: 120px;
+    min-width: 182px;
+    background-position: center;
+    background-size: cover;
+  }
+
+  .text {
+    margin: 10px;
+  }
+
+  h2 {
+    font-size: 18px;
+  }
+
+  p {
+    font-size: 14px;
+    opacity: 0.8;
+  }
+</style>
