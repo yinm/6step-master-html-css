9fb843bf99a4405a6cd6e547d11c0f18c9f8bacc
yinm <yinm.cminor9@gmail.com>
2018-10-29 21:15:27 +0900

    Try 5-1-2

---
+++ trace/components/Card.vue
@@ -0,0 +1,45 @@
+<template>
+  <a href="#">
+    <div class="photo" />
+    <div class="text-container">
+      <h2>黄色い花を飾ってみた</h2>
+      <p>黄色い花を部屋に飾るといいことがあるかもしれません。その理由は…</p>
+    </div>
+  </a>
+</template>
+
+<style scoped>
+  a {
+    display: block;
+    margin: 10px;
+    border: solid 1px var(--gray-color);
+    text-decoration: none;
+    color: inherit;
+  }
+
+  a:hover {
+    opacity: 0.8;
+  }
+
+  .photo {
+    height: 150px;
+    width: 100%;
+    background-image: url('/p01.jpg');
+    background-position: center;
+    background-size: cover;
+  }
+
+  .text-container {
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+
+  h2 {
+    font-size: 18px;
+  }
+
+  p {
+    font-size: 14px;
+    opacity: 0.7;
+  }
+</style>
--- trace/pages/articleA.vue
+++ trace/pages/articleA.vue
@@ -2,17 +2,20 @@
   <div>
     <Header />
     <Hero2 />
+    <Card />
     <Footer />
   </div>
 </template>
 
 <script>
+  import Card from '~/components/Card'
   import Footer from '~/components/Footer'
   import Header from '~/components/Header'
   import Hero2 from '~/components/Hero2'
 
   export default {
     components: {
+      Card,
       Footer,
       Header,
       Hero2,
