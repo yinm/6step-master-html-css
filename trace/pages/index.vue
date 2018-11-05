17e6602d4b23f1e2b8809731d9c6d69fbb6bdee3
yinm <yinm.cminor9@gmail.com>
2018-11-05 22:07:21 +0900

    Try 2-1-1

--- trace/pages/index.vue
+++ trace/pages/index.vue
@@ -1,37 +1,22 @@
 <template>
   <div>
-    <Header />
     <Hero />
-    <Concept />
-    <Content />
-    <ReversedContent />
-    <Footer />
   </div>
 </template>
 
 <script>
-import Hero from '~/components/Hero.vue'
-import Concept from '~/components/Concept.vue'
-import Content from '~/components/Content.vue'
-import Header from '~/components/Header.vue'
-import ReversedContent from '~/components/ReversedContent.vue'
-import Footer from '~/components/Footer.vue'
+  import Hero from '~/components/Hero'
 
-export default {
-  components: {
-    Hero,
-    Concept,
-    Content,
-    Header,
-    ReversedContent,
-    Footer
+  export default {
+    components: {
+      Hero,
+    }
   }
-}
 </script>
 
 <style>
-body {
-  margin: 0;
-  font-family: 'メイリオ', 'Hiragino Kaku Gothic Pro', sans-serif;
-}
+  body {
+    margin: 0;
+    font-family: 'メイリオ', 'Hiragino Kaku Gothic Pro', sans-serif;
+  }
 </style>
