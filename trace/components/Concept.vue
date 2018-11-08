37b80493a8cbe9ab21efeb37ed70e82e0f1ac6e5
yinm <yinm.cminor9@gmail.com>
2018-11-08 21:16:50 +0900

    Try 2-2-3

--- trace/components/Concept.vue
+++ trace/components/Concept.vue
@@ -10,11 +10,33 @@
           <i class="fas fa-chevron-right" />
         </a>
       </div>
+      <div class="text">
+        <i class="fas fa-cogs icon" />
+        <h2>必要なのはスマホだけ</h2>
+        <p>ライフログを始めるのに必要なのはスマホだけ。いつでもスタートできます。</p>
+        <a href="#">
+          MORE...
+          <i class="fas fa-chevron-right" />
+        </a>
+      </div>
+      <div class="text">
+        <i class="fas fa-rocket icon" />
+        <h2>SNSとの連携も簡単</h2>
+        <p>TwitterやFacebookといったSNSと連携して、簡単にログを共有することができます。</p>
+        <a href="#">
+          MORE...
+          <i class="fas fa-chevron-right" />
+        </a>
+      </div>
     </div>
   </section>
 </template>
 
 <style scoped>
+  .container {
+    padding: 80px 20px;
+  }
+
   .icon {
     display: inline-block;
     margin-bottom: 20px;
@@ -29,6 +51,10 @@
 
   .text {
     text-align: center;
+  }
+
+  .text:not(:last-child) {
+    margin-bottom: 60px;
   }
 
   h2 {
@@ -52,5 +78,18 @@
 
   a:hover {
     text-decoration: underline;
+  }
+
+  @media (min-width: 768px) {
+    .container {
+      display: flex;
+      margin: 0 auto;
+      padding-bottom: 0;
+      max-width: var(--large-width);
+    }
+
+    .text {
+      flex: 1;
+    }
   }
 </style>
