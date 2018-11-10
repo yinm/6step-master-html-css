b969f8d4603b966e74b000552faa011c534f83b7
yinm <yinm.cminor9@gmail.com>
2018-11-10 16:32:58 +0900

    Try 3-2-1

--- trace/components/Footer.vue
+++ trace/components/Footer.vue
@@ -6,6 +6,22 @@
         〒000-0000 東京都中央区杉並3-3-3-403<br>
         <a href="#">http://logger.nett/</a>
       </p>
+
+      <h3>ABOUT</h3>
+      <ul>
+        <li>
+          <a href="#">設立</a>
+        </li>
+        <li>
+          <a href="#">所在地</a>
+        </li>
+        <li>
+          <a href="#">地図</a>
+        </li>
+        <li>
+          <a href="#">スタッフ</a>
+        </li>
+      </ul>
     </div>
   </footer>
 </template>
@@ -37,5 +53,32 @@
     color: inherit;
     font-size: 14px;
     text-decoration: none;
+  }
+
+  h3 {
+    margin-top: 30px;
+    margin-bottom: 10px;
+    border-bottom: solid 1px currentColor;
+    font-size: 14px;
+  }
+
+  ul {
+    margin-top: 0;
+    margin-bottom: 20px;
+    padding-left: 0;
+    list-style-type: none;
+  }
+
+  li a {
+    display: block;
+    padding: 5px;
+    font-size: 12px;
+  }
+
+  li a:hover {
+    background-image: linear-gradient(
+      rgba(0, 0, 0, 0.3),
+      rgba(0, 0, 0, 0.3)
+    );
   }
 </style>
