a95b18602d8d5e83b19639ddac8e384de164dc37
yinm <yinm.cminor9@gmail.com>
2018-11-10 13:41:32 +0900

    Check sample

--- trace/components/Content.vue
+++ trace/components/Content.vue
@@ -2,28 +2,18 @@
   <section class="conC">
     <div class="container">
       <div class="photo" />
-      <div class="text">
-        <h2>面倒な設定やデータの管理は全部おまかせ</h2>
-        <p>ライフログでつまずきがちな面倒な設定やデータ管理の煩わしさはありません。他のログアプリとの連携もワンクリックで完了です。</p>
-        <a href="#">
-          詳しい設定方法を確認する
-          <i class="fas fa-chevron-right" />
-        </a>
-      </div>
+      <h2>面倒な設定やデータの管理は全部おまかせ</h2>
+      <p>ライフログでつまずきがちな面倒な設定やデータ管理の煩わしさはありません。他のログアプリとの連携もワンクリックで完了です。</p>
+      <a href="#">
+        詳しい設定方法を確認する
+        <i class="fas fa-chevron-right" />
+      </a>
     </div>
   </section>
 </template>
 
 <style scoped>
-  .photo {
-    height: 200px;
-    width: 100%;
-    background-image: url("/note.jpg");
-    background-position: center;
-    background-size: cover;
-  }
-
-  .text {
+  .conC {
     background-color: var(--main-color);
     color: var(--text-bright-color);
   }
@@ -36,7 +26,7 @@
 
   p {
     margin-top: 0;
-    margin-bottom: 40px;
+    margin-bottom: 20px;
     font-size: 14px;
     line-height: 1.8;
     opacity: 0.8;
@@ -44,13 +34,14 @@
 
   a {
     display: inline-block;
+    margin-top: 20px;
     padding: 10px 30px;
     border: solid 3px currentColor;
     border-radius: 6px;
     background-color: var(--accent-color);
-    text-decoration: none;
-    color: inherit;
+    color: var(--text-bright-color);
     font-size: 14px;
+    text-decoration: none;
   }
 
   a:hover {
@@ -58,5 +49,12 @@
       rgba(255, 255, 255, 0.2),
       rgba(255, 255, 255, 0.2)
     );
+  }
+
+  .photo {
+    min-height: 200px;
+    background-image: url(/note.jpg);
+    background-position: center;
+    background-size: cover;
   }
 </style>
