cee16e1877fa449069d4f810dfca705896478cef
yinm <yinm.cminor9@gmail.com>
2018-11-12 21:15:51 +0900

    Try 3-5

---
+++ trace/components/LoggerHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <div class="container">
+      <h2>LOGGER</h2>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  position: absolute;
+  background-color: #fff;
+}
+
+h2 {
+  display: inline-block;
+  margin: 0;
+  padding: 20px;
+  font-family: 'Montserrat', sans-serif;
+  font-weight: normal;
+  background-color: var(--main-color);
+  color: var(--text-bright-color);
+}
+</style>
--- trace/pages/index.vue
+++ trace/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <logger-header />
     <Hero />
     <Concepts />
     <Content />
@@ -13,6 +14,7 @@
   import Concepts from '~/components/Concepts'
   import Content from '~/components/Content'
   import Footer from '~/components/Footer'
+  import LoggerHeader from '~/components/LoggerHeader'
   import ReversedContent from '~/components/ReversedContent'
 
   export default {
@@ -21,25 +23,26 @@
       Concepts,
       Content,
       Footer,
+      LoggerHeader,
       ReversedContent
     }
   }
 </script>
 
 <style>
-  body {
-    margin: 0;
-    font-family: 'メイリオ', 'Hiragino Kaku Gothic Pro', sans-serif;
-  }
+body {
+  margin: 0;
+  font-family: 'メイリオ', 'Hiragino Kaku Gothic Pro', sans-serif;
+}
 
-  .sr-only {
-    position: absolute;
-    width: 1px;
-    height: 1px;
-    padding: 0;
-    margin: -1px;
-    overflow: hidden;
-    clip: rect(0, 0, 0, 0);
-    border: 0;
-  }
+.sr-only {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  border: 0;
+}
 </style>
