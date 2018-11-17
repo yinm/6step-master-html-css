74c82b87d1244c2b557b21c94d89d4d25579d8c4
yinm <yinm.cminor9@gmail.com>
2018-11-17 13:41:47 +0900

    Try 4-3-1

---
+++ trace/components/ContactView.vue
@@ -0,0 +1,108 @@
+<template>
+  <div>
+    <div class="container">
+      <h1>お問い合わせ</h1>
+      <p>ROGGERについてのご質問などがございましたら、下記の窓口よりお問い合わせください。</p>
+      <div class="contact-wrap">
+        <div class="card">
+          <i class="fas fa-phone" />
+          <h2>電話</h2>
+          <p>000-1234-5678</p>
+        </div>
+        <div class="card">
+          <i class="fas fa-envelope" />
+          <h2>Eメール</h2>
+          <p>contact@logger.nett</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  max-width: var(--middle-width);
+  margin-left: auto;
+  margin-right: auto;
+  padding: 50px 10px;
+}
+
+@media (max-width: 599px) {
+  :root {
+    font-size: 14px;
+  }
+}
+
+@media (min-width: 600px) and (max-width: 799px) {
+  :root {
+    font-size: 16px;
+  }
+}
+
+@media (min-width: 800px) {
+  :root {
+    font-size: 18px;
+  }
+}
+
+body {
+  font-size: 16px;
+}
+
+h1 {
+  margin-top: 10px;
+  margin-bottom: 2rem;
+  border-left: solid 0.75rem var(--accent-color);
+  padding-left: 0.5rem;
+  font-size: 2rem;
+}
+
+p {
+  margin-top: 0;
+  font-size: 1rem;
+}
+
+.contact-wrap {
+  margin-top: 30px;
+}
+
+.card {
+  margin-top: 20px;
+  padding: 20px;
+  border: solid 1px var(--gray-color);
+  text-align: center;
+}
+
+i {
+  border-radius: 50%;
+  background-color: var(--main-color);
+  color: var(--icon-color);
+  font-size: 2.2rem;
+  width: 2em;
+  line-height: 2em;
+  text-align: center;
+}
+
+h2 {
+  font-size: 1.2rem;
+}
+
+.card p {
+  margin: 0;
+  font-size: 1.4rem;
+}
+
+@media (min-width: 800px) {
+  .contact-wrap {
+    display: flex;
+  }
+
+  .card {
+    flex: 1;
+  }
+
+  .card:not(:last-child) {
+    margin-right: 20px;
+  }
+}
+</style>
---
+++ trace/pages/contact.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <logger-header :is-no-hero-image="true" />
+    <contact-view />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import ContactView from '~/components/ContactView'
+import Footer from '~/components/Footer'
+import LoggerHeader from '~/components/LoggerHeader'
+
+export default {
+  components: {
+    ContactView,
+    Footer,
+    LoggerHeader
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: 'メイリオ', 'Hiragino Kaku Gothic Pro', sans-serif;
+}
+
+.sr-only {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  border: 0;
+}
+</style>
