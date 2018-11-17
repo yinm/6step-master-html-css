69682540fdebe181f680ab7d39ddd67565486995
yinm <yinm.cminor9@gmail.com>
2018-11-17 12:14:06 +0900

    Try 4-2-2

--- trace/components/AboutContent.vue
+++ trace/components/AboutContent.vue
@@ -5,8 +5,19 @@
       <p>LOGGERはライフログに関する情報の発信、アプリ開発、データの分析、ユーザーサポートを行うために設立された会社です。ライフログのサービスやデータを活用することで、日々の生活を楽しみ、よりよくしていくことを目指しています。</p>
       <p>近年はグローバルワールドネットワークとも提携し、国内のみならず、海外展開も含めて活動の場をひろげています。</p>
     </div>
+    <about-history />
   </section>
 </template>
+
+<script>
+import AboutHistory from './AboutHistory'
+
+export default {
+  components: {
+    AboutHistory
+  }
+}
+</script>
 
 <style scoped>
 .container {
---
+++ trace/components/AboutHistory.vue
@@ -0,0 +1,85 @@
+<template>
+  <section>
+    <div class="container">
+      <div class="photo" />
+      <div class="history-container">
+        <h2>History</h2>
+        <table>
+          <tr>
+            <td>2010年4月</td>
+            <td>LOGGER設立</td>
+          </tr>
+          <tr>
+            <td>2012年6月</td>
+            <td>オンラインマガジン創刊</td>
+          </tr>
+          <tr>
+            <td>2014年12月</td>
+            <td>海外企業と提携してオンラインデータ共有事業をスタート</td>
+          </tr>
+          <tr>
+            <td>2015年1月</td>
+            <td>ライフログアプリをリリース</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+section {
+  background-color: var(--main-color);
+  color: var(--text-bright-color);
+}
+
+.photo {
+  min-height: 300px;
+  background-image: url(/plant.jpg);
+  background-position: center;
+  background-size: cover;
+}
+
+.history-container {
+  padding: 10px 20px;
+}
+
+h2 {
+  margin-bottom: 7px;
+}
+
+tr {
+  display: block;
+  padding-top: 30px;
+  padding-bottom: 25px;
+  border-top: solid 1px currentColor;
+  font-size: 0.875rem;
+}
+
+tr:last-child {
+  padding-bottom: 30px;
+  border-bottom: solid 1px currentColor;
+}
+
+td:first-child {
+  font-weight: bold;
+  min-width: 110px;
+}
+
+@media (min-width: 768px) {
+  .container {
+    display: flex;
+    padding: 0 30px;
+    justify-content: space-between;
+  }
+
+  .photo {
+    flex: 3;
+  }
+
+  .history-container {
+    flex: 2;
+    padding: 30px 50px;
+  }
+}
+</style>
