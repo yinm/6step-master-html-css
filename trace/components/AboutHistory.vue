b5209d6418ce20a975d2ca1111b5543669894737
yinm <yinm.cminor9@gmail.com>
2018-11-17 12:27:12 +0900

    Check sample

--- trace/components/AboutHistory.vue
+++ trace/components/AboutHistory.vue
@@ -1,36 +1,46 @@
 <template>
-  <section>
+  <aside class="history">
     <div class="container">
       <div class="photo" />
-      <div class="history-container">
+      <div class="text">
         <h2>History</h2>
         <table>
           <tr>
-            <td>2010年4月</td>
+            <th>2010年4月</th>
             <td>LOGGER設立</td>
           </tr>
           <tr>
-            <td>2012年6月</td>
+            <th>2012年6月</th>
             <td>オンラインマガジン創刊</td>
           </tr>
           <tr>
-            <td>2014年12月</td>
+            <th>2014年12月</th>
             <td>海外企業と提携してオンラインデータ共有事業をスタート</td>
           </tr>
           <tr>
-            <td>2015年1月</td>
+            <th>2015年1月</th>
             <td>ライフログアプリをリリース</td>
           </tr>
         </table>
       </div>
     </div>
-  </section>
+  </aside>
 </template>
 
 <style scoped>
-section {
+.history {
   background-color: var(--main-color);
   color: var(--text-bright-color);
+}
+
+.text {
+  padding: 20px;
+}
+
+h2 {
+  margin-top: 0;
+  margin-bottom: 10px;
+  font-size: 1.5rem;
 }
 
 .photo {
@@ -40,46 +50,40 @@ section {
   background-size: cover;
 }
 
-.history-container {
-  padding: 10px 20px;
-}
-
-h2 {
-  margin-bottom: 7px;
-}
-
-tr {
-  display: block;
-  padding-top: 30px;
-  padding-bottom: 25px;
-  border-top: solid 1px currentColor;
+table {
+  border-collapse: collapse;
+  border-top: solid 1px var(--gray-color);
   font-size: 0.875rem;
 }
 
-tr:last-child {
-  padding-bottom: 30px;
-  border-bottom: solid 1px currentColor;
+th,
+td {
+  padding-top: 1.8rem;
+  padding-bottom: 1.8rem;
+  border-bottom: solid 1px var(--gray-color);
 }
 
-td:first-child {
-  font-weight: bold;
-  min-width: 110px;
+th {
+  padding-right: 1rem;
+  text-align: left;
+  word-break: keep-all;
 }
 
 @media (min-width: 768px) {
   .container {
     display: flex;
-    padding: 0 30px;
-    justify-content: space-between;
+    max-width: var(--large-width);
+    margin-left: auto;
+    margin-right: auto;
   }
 
   .photo {
     flex: 3;
   }
 
-  .history-container {
+  .text {
     flex: 2;
-    padding: 30px 50px;
+    padding: 50px;
   }
 }
 </style>
