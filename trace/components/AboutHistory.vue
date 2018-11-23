8a2315f57012b632a2643d8f5e824b1ed1fc344b
yinm <yinm.cminor9@gmail.com>
2018-11-23 17:06:38 +0900

    Check sample

--- trace/components/AboutHistory.vue
+++ trace/components/AboutHistory.vue
@@ -1,27 +1,28 @@
 <template>
   <aside class="history">
-    <div class="photo" />
-
-    <div class="text">
-      <h2>History</h2>
-      <table>
-        <tr>
-          <th>2010年4月</th>
-          <td>LOGGER設立</td>
-        </tr>
-        <tr>
-          <th>2012年6月</th>
-          <td>オンラインマガジン創刊</td>
-        </tr>
-        <tr>
-          <th>2014年12月</th>
-          <td>海外企業と提携してオンラインデータ共有事業をスタート</td>
-        </tr>
-        <tr>
-          <th>2015年1月</th>
-          <td>ライフログアプリをリリース</td>
-        </tr>
-      </table>
+    <div class="container">
+      <div class="photo" />
+      <div class="text">
+        <h2>History</h2>
+        <table>
+          <tr>
+            <th>2010年4月</th>
+            <td>LOGGER設立</td>
+          </tr>
+          <tr>
+            <th>2012年6月</th>
+            <td>オンラインマガジン創刊</td>
+          </tr>
+          <tr>
+            <th>2014年12月</th>
+            <td>海外企業と提携してオンラインデータ共有事業をスタート</td>
+          </tr>
+          <tr>
+            <th>2015年1月</th>
+            <td>ライフログアプリをリリース</td>
+          </tr>
+        </table>
+      </div>
     </div>
   </aside>
 </template>
@@ -32,6 +33,16 @@
   color: var(--text-bright-color);
 }
 
+.text {
+  padding: 20px;
+}
+
+h2 {
+  margin-top: 0;
+  margin-bottom: 10px;
+  font-size: 1.5rem;
+}
+
 .photo {
   min-height: 300px;
   background-image: url(/plant.jpg);
@@ -39,44 +50,31 @@
   background-size: cover;
 }
 
-.text {
-  padding: 20px;
-}
-
 table {
-  border-top: solid 1px var(--gray-color);
   border-collapse: collapse;
-}
-
-h2 {
-  margin-top: 0;
-  margin-bottom: 10px;
-}
-
-tr {
-  height: 90px;
-  font-size: 0.87rem;
+  border-top: solid 1px var(--gray-color);
+  font-size: 0.875rem;
 }
 
 th,
 td {
+  padding-top: 1.8rem;
+  padding-bottom: 1.8rem;
   border-bottom: solid 1px var(--gray-color);
 }
 
 th {
-  word-break: keep-all;
+  padding-right: 1rem;
   text-align: left;
-}
-
-td {
-  padding-left: 1rem;
+  word-break: keep-all;
 }
 
 @media (min-width: 768px) {
-  .history {
+  .container {
     display: flex;
-    padding-left: 35px;
-    padding-right: 35px;
+    max-width: var(--large-width);
+    margin-left: auto;
+    margin-right: auto;
   }
 
   .photo {
