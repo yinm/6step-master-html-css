ad7095d0ebbac676aa4b50acd60371ecc6944379
yinm <yinm.cminor9@gmail.com>
2018-11-23 16:21:56 +0900

    Try 4-2-1

--- trace/components/AboutContent.vue
+++ trace/components/AboutContent.vue
@@ -1,32 +1,12 @@
 <template>
-  <section>
-    <div class="container">
-      <h1>LOGGERについて</h1>
-      <p>LOGGERはライフログに関する情報の発信、アプリ開発、データの分析、ユーザーサポートを行うために設立された会社です。ライフログのサービスやデータを活用することで、日々の生活を楽しみ、よりよくしていくことを目指しています。</p>
-      <p>近年はグローバルワールドネットワークとも提携し、国内のみならず、海外展開も含めて活動の場をひろげています。</p>
-    </div>
-    <about-history />
-  </section>
+  <div class="container">
+    <h1>LOGGERについて</h1>
+    <p>LOGGERはライフログに関する情報の発信、アプリ開発、データの分析、ユーザーサポートを行うために設立された会社です。ライフログのサービスやデータを活用することで、日々の生活を楽しみ、よりよくしていくことを目指しています。</p>
+    <p>近年はグローバルワールドネットワークとも提携し、国内のみならず、海外展開も含めて活動の場をひろげています。</p>
+  </div>
 </template>
 
-<script>
-import AboutHistory from './AboutHistory'
-
-export default {
-  components: {
-    AboutHistory
-  }
-}
-</script>
-
 <style scoped>
-.container {
-  padding: 30px 10px;
-  max-width: 800px;
-  margin-left: auto;
-  margin-right: auto;
-}
-
 @media (max-width: 599px) {
   :root {
     font-size: 14px;
@@ -49,7 +29,16 @@ body {
   font-size: 16px;
 }
 
+.container {
+  max-width: var(--middle-width);
+  margin-left: auto;
+  margin-right: auto;
+  padding: 30px 10px;
+}
+
 h1 {
+  margin-top: 30px;
+  margin-bottom: 30px;
   border-left: solid 0.75rem var(--accent-color);
   padding-left: 0.5rem;
   font-size: 2rem;
@@ -60,3 +49,4 @@ p {
   line-height: 2;
 }
 </style>
+
